c50d78d12ffacbf748ad94de8440813fc11aeaf0
Patrik Björk <patrik.bjork@codespirit.se>
2020-06-04 14:50:32 +0200

    Use Redis cluster configuration.

--- core-bc/modules/web/src/main/java/se/_1177/lmn/configuration/session/RedisSessionConfig.java
+++ core-bc/modules/web/src/main/java/se/_1177/lmn/configuration/session/RedisSessionConfig.java
@@ -1,10 +1,12 @@
 package se._1177.lmn.configuration.session;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import se._1177.lmn.service.util.LakemedelsnaraProperties;
+
+import java.util.Arrays;
 
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600) // One hour, which is more than session timeout for fronting web servers.
 public class RedisSessionConfig {
@@ -14,10 +16,7 @@ public class RedisSessionConfig {
 		LakemedelsnaraProperties properties = LakemedelsnaraProperties.getInstance();
 
 		return new LettuceConnectionFactory(
-				new RedisStandaloneConfiguration(
-						properties.get("spring.redis.host"),
-						Integer.parseInt(properties.get("spring.redis.port"))
-				)
+				new RedisClusterConfiguration(Arrays.asList(properties.get("spring.redis.cluster.nodes").split(",")))
 		);
 	}
 }
