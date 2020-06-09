e0e2bc36b8a638d830c085dd3c1cb23eb1e161da
Patrik Björk <patrik.bjork@codespirit.se>
2020-06-09 20:42:23 +0200

    Use RedisSentinelConfiguration. Set version to 4.0.2.

--- core-bc/modules/web/src/main/java/se/_1177/lmn/configuration/session/RedisSessionConfig.java
+++ core-bc/modules/web/src/main/java/se/_1177/lmn/configuration/session/RedisSessionConfig.java
@@ -1,13 +1,15 @@
 package se._1177.lmn.configuration.session;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import se._1177.lmn.service.util.LakemedelsnaraProperties;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600) // One hour, which is more than session timeout for fronting web servers.
@@ -18,6 +20,7 @@ public class RedisSessionConfig {
 		LakemedelsnaraProperties properties = LakemedelsnaraProperties.getInstance();
 
 		List<String> clusterNodes = Arrays.asList(properties.get("spring.redis.cluster.nodes").split(","));
+		String sentinelMaster = properties.get("spring.redis.sentinel.master");
 
 		if (clusterNodes.size() == 1) {
 			String[] hostAndPort = clusterNodes.get(0).split(":");
@@ -28,7 +31,9 @@ public class RedisSessionConfig {
 			return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
 		}
 
-		return new LettuceConnectionFactory(new RedisClusterConfiguration(clusterNodes));
+		return new LettuceConnectionFactory(
+				new RedisSentinelConfiguration(sentinelMaster, new HashSet<>(clusterNodes))
+		);
 	}
 }
 
